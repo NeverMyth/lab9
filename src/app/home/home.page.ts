9acb3b3696783576eaa71759de39091084560af1
NeverMyth <hemersondc96@gmail.com>
2023-04-20 14:44:09 +0000

    lab9

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import{Storage} from '@ionic/storage-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
+myStatus:any[]=[];
+  constructor(private storage:Storage) {}
   
+
+async ionViewWillEnter(){
+  await this.storage.create();
+  this.myStatus = await this.storage.get('status')
 }
+}
+
